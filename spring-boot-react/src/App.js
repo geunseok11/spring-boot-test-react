3cfc7af8ba0d1a661e7ed3c7c6befea17f97cecd
geunseok11 <junggeunseok11@gmail.com>
2022-04-08 22:03:38 +0900

    ADD setupProxy

--- spring-boot-react/src/App.js
+++ spring-boot-react/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from axios;
+import React, {useState, useEffect} from "react";
 
 function App() {
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    axios.post("/api/users").then((res)=>{
+      if(res.data){
+        setUser(res.data);
+      }else{
+        alert("failed")
+      }
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{user.id}</h1>
+        <h1>{user.username}</h1>
       </header>
     </div>
   );
